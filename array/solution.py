41510a4b1d416116e3434aa66386de7ea574814f
Pilhwan Kim <pilhwankim03@gmail.com>
2020-02-21 09:50:54 +0900

    카테고리별 코드 정리

--- array/solution.py
+++ array/solution.py
@@ -2,6 +2,30 @@ from typing import List
 
 
 class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        temp = None
+        while True:
+            if len(nums) <= i:
+                break
+
+            if temp == nums[i]:
+                del nums[i]
+            else:
+                temp = nums[i]
+                i = i + 1
+
+        return len(nums)
+
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for index in range(1, len(prices)):
+            margin = prices[index] - prices[index - 1]
+            if margin > 0:
+                profit += margin
+
+        return profit if profit >= 0 else 0
+
     def containsDuplicate(self, nums: List[int]) -> bool:
         nums_set = set(nums)
         if len(nums) == len(nums_set):
@@ -80,9 +104,3 @@ class Solution:
                 subbox_set.add(cell_int)
 
         return True
-
-
-if __name__ == "__main__":
-    # ret = Solution().removeDuplicates([0,0,1,1,1,2,2,3,3,4])
-    ret = Solution().containsDuplicate([7, 1, 5, 3, 6, 4])
-    print(ret)
