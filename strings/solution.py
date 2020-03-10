73849a39e5dd912ffe439106510722bc3297b573
Pilhwan Kim <pilhwankim03@gmail.com>
2020-03-10 10:22:18 +0900

    add reverseInteger

--- strings/solution.py
+++ strings/solution.py
@@ -5,3 +5,16 @@ import math
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         s[::] = s[-1::-1]
+
+    def reverse(self, x: int) -> int:
+        reverse_str = str(x)[::-1]
+
+        if reverse_str[-1] == '-':
+            num_int = - int(reverse_str[:-1])
+        else:
+            num_int = int(reverse_str)
+
+        if (1 << 31)-1 < num_int or (-1 << 31) > num_int:
+            return 0
+
+        return num_int
--- strings/tests.py
+++ strings/tests.py
@@ -5,3 +5,9 @@ def test_reverseString():
     assert Solution().reverseString(["h", "e", "l", "l", "o"]) == ["o", "l", "l", "e", "h"]
     assert Solution().reverseString(["H", "a", "n", "n", "a", "h"]) == ["h", "a", "n", "n", "a", "H"]
     assert Solution().reverseString(["h", "e", "l", "l", "o"]) == ["o", "l", "l", "e", "h"]
+
+
+def test_reverse():
+    assert Solution().reverse(123) == 321
+    assert Solution().reverse(-123) == -321
+    assert Solution().reverse(120) == 21
